d7cb09518e84dc22084ae7f34942beed825bb046
Han <gaohan.henry@Gmail.com>
2016-10-16 16:09:50 -0700

    implement result page

--- public/js/app.js
+++ public/js/app.js
@@ -128,3 +128,104 @@ $(".back ul li").on("click", function(){
     $("span").html(val);
 });
 //drop down end
+
+
+
+//login popup
+$(function(){
+    $('.sign-a').on('click', function(){
+        $('.form1').show();
+        $('.back-grey').show();
+    });
+    $('.close-popup').on('click', function(){
+
+    });
+
+});
+
+$(document).mouseup(function(e){
+    var _con = $('.form1');
+    if(!_con.is(e.target) && _con.has(e.target).length === 0){
+        $('.form1').hide();
+        $('.back-grey').hide();
+    }
+});
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+    var $this = $(this),
+        label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if( $this.val() === '' ) {
+            label.removeClass('active highlight');
+        } else {
+            label.removeClass('highlight');
+        }
+    } else if (e.type === 'focus') {
+
+        if( $this.val() === '' ) {
+            label.removeClass('highlight');
+        }
+        else if( $this.val() !== '' ) {
+            label.addClass('highlight');
+        }
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+});
+//login popup end
+
+
+
+//google map
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var myLatLng = {lat: 49.246292, lng: -123.116226};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: true,
+        zoom: 11
+
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+
+
+    var autocomplete = new google.maps.places.Autocomplete(
+        (
+            document.getElementById('search_text')), {
+            types: ['(cities)']
+        });
+    places = new google.maps.places.PlacesService(map);
+
+
+
+}
+//google map end
+
